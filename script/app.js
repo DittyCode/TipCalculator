06e9441f934825cc305124d9868657eec919dfb4
Kamil <kam.nas21@wp.pl>
2022-01-02 10:58:00 +0100

    #4 Typescript logic coding

--- script/app.js
+++ script/app.js
@@ -1,1 +1,100 @@
-console.log('Hello World!');
+var Tip;
+(function (Tip) {
+    Tip[Tip["5%"] = 5] = "5%";
+    Tip[Tip["10%"] = 10] = "10%";
+    Tip[Tip["15%"] = 15] = "15%";
+    Tip[Tip["25%"] = 25] = "25%";
+    Tip[Tip["50%"] = 50] = "50%";
+})(Tip || (Tip = {}));
+var billInput = document.querySelector('[data-bill]');
+var peopleInput = document.querySelector('[data-people]');
+var tipAmount = document.querySelector('[data-amount]');
+var tipTotal = document.querySelector('[data-total]');
+var tipCustom = document.querySelector('[data-custom]');
+var resetButton = document.querySelector('[data-reset]');
+var buttons = document.querySelectorAll('[data-tip]');
+var values = {
+    bill: 0,
+    people: 0,
+    tip: 0
+};
+var generateTipValue = function (_a) {
+    var target = _a.target;
+    var targetTipButton = target;
+    var targetTip = targetTipButton.textContent;
+    if (targetTip) {
+        values.tip = Tip[targetTip];
+    }
+};
+var generateBillValue = function (_a) {
+    var target = _a.target;
+    var targetBillInput = target;
+    var bill = targetBillInput.value;
+    if (bill) {
+        values.bill = +bill.replace(/,/g, '.');
+    }
+};
+var generatePeopleValue = function (_a) {
+    var target = _a.target;
+    var targetPeopleInput = target;
+    var people = targetPeopleInput.value;
+    if (people) {
+        values.people = +people;
+    }
+};
+var validValues = function () {
+    var valid = [];
+    var valuesKey;
+    for (valuesKey in values) {
+        valid.push(values[valuesKey]);
+    }
+    return valid;
+};
+var generateValues = function () {
+    var tip = values.tip, people = values.people, bill = values.bill;
+    var roundedTip = ((bill * tip) / 100 / people).toFixed(2);
+    var total = (+(bill / people).toFixed(2) + +roundedTip).toFixed(2);
+    return { total: total, roundedTip: roundedTip };
+};
+var generateTipCustomValue = function (_a) {
+    var target = _a.target;
+    var targetTipCustomInput = target;
+    values.tip = +targetTipCustomInput.value;
+};
+var initProject = function () {
+    var valuesKey;
+    for (valuesKey in values) {
+        values[valuesKey] = 0;
+    }
+    [billInput, peopleInput].forEach(function (input) { return (input.value = ''); });
+    [tipAmount, tipTotal].forEach(function (tip) { return (tip.textContent = "$0.00"); });
+};
+var generateHTML = function () {
+    var generatedValidValues = validValues();
+    if (generatedValidValues.indexOf(0) === -1) {
+        var _a = generateValues(), total = _a.total, roundedTip = _a.roundedTip;
+        tipAmount.textContent = roundedTip;
+        tipTotal.textContent = total;
+    }
+};
+peopleInput.addEventListener('input', function (e) {
+    generatePeopleValue(e);
+    generateHTML();
+});
+billInput.addEventListener('input', function (e) {
+    generateBillValue(e);
+    generateHTML();
+});
+tipCustom.addEventListener('input', function (e) {
+    generateTipCustomValue(e);
+    generateHTML();
+});
+resetButton.addEventListener('click', function () {
+    initProject();
+});
+buttons.forEach(function (button) {
+    return button.addEventListener('click', function (e) {
+        generateTipValue(e);
+        generateHTML();
+    });
+});
--- script/app.ts
+++ script/app.ts
@@ -1,1 +1,117 @@
-console.log('Hello World!');
+interface Values {
+	bill: number;
+	people: number;
+	tip: number;
+}
+
+enum Tip {
+	'5%' = 5,
+	'10%' = 10,
+	'15%' = 15,
+	'25%' = 25,
+	'50%' = 50,
+}
+
+const billInput = document.querySelector('[data-bill]') as HTMLInputElement;
+const peopleInput = document.querySelector('[data-people]') as HTMLInputElement;
+const tipAmount = document.querySelector('[data-amount]') as HTMLHeadingElement;
+const tipTotal = document.querySelector('[data-total]') as HTMLHeadingElement;
+const tipCustom = document.querySelector('[data-custom]') as HTMLInputElement;
+const resetButton = document.querySelector('[data-reset]') as HTMLButtonElement;
+
+const buttons = document.querySelectorAll('[data-tip]');
+
+const values: Values = {
+	bill: 0,
+	people: 0,
+	tip: 0,
+};
+
+const generateTipValue = ({ target }: Event) => {
+	const targetTipButton = target as HTMLButtonElement;
+	const targetTip = targetTipButton.textContent;
+	if (targetTip) {
+		values.tip = Tip[targetTip];
+	}
+};
+
+const generateBillValue = ({ target }: Event) => {
+	const targetBillInput = target as HTMLInputElement;
+	const bill = targetBillInput.value;
+	if (bill) {
+		values.bill = +bill.replace(/,/g, '.');
+	}
+};
+
+const generatePeopleValue = ({ target }: Event) => {
+	const targetPeopleInput = target as HTMLInputElement;
+	const people = targetPeopleInput.value;
+	if (people) {
+		values.people = +people;
+	}
+};
+
+const validValues = () => {
+	const valid = [];
+	let valuesKey: keyof typeof values;
+	for (valuesKey in values) {
+		valid.push(values[valuesKey]);
+	}
+	return valid;
+};
+
+const generateValues = () => {
+	const { tip, people, bill } = values;
+	const roundedTip = ((bill * tip) / 100 / people).toFixed(2);
+	const total = (+(bill / people).toFixed(2) + +roundedTip).toFixed(2);
+	return { total, roundedTip };
+};
+
+const generateTipCustomValue = ({ target }: Event) => {
+	const targetTipCustomInput = target as HTMLInputElement;
+	values.tip = +targetTipCustomInput.value;
+};
+
+const initProject = () => {
+	let valuesKey: keyof typeof values;
+	for (valuesKey in values) {
+		values[valuesKey] = 0;
+	}
+	[billInput, peopleInput].forEach(input => (input.value = ''));
+	[tipAmount, tipTotal].forEach(tip => (tip.textContent = `$0.00`));
+};
+
+const generateHTML = () => {
+	const generatedValidValues = validValues();
+	if (generatedValidValues.indexOf(0) === -1) {
+		const { total, roundedTip } = generateValues();
+		tipAmount.textContent = roundedTip;
+		tipTotal.textContent = total;
+	}
+};
+
+peopleInput.addEventListener('input', e => {
+	generatePeopleValue(e);
+	generateHTML();
+});
+
+billInput.addEventListener('input', e => {
+	generateBillValue(e);
+	generateHTML();
+});
+
+tipCustom.addEventListener('input', e => {
+	generateTipCustomValue(e);
+	generateHTML();
+});
+
+resetButton.addEventListener('click', () => {
+	initProject();
+});
+
+buttons.forEach(button =>
+	button.addEventListener('click', e => {
+		generateTipValue(e);
+		generateHTML();
+	})
+);
